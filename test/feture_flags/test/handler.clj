23c9d627b6b70211be8619f51001497736d19d42
nils janson <nils.janson@nordstrom.com>
2014-06-18 21:05:49 -0700

    Added possitive tests for the flag routes.

--- test/feture_flags/test/handler.clj
+++ test/feture_flags/test/handler.clj
@@ -1,9 +1,20 @@
 (ns feture-flags.test.handler
   (:use clojure.test
         ring.mock.request  
-        feture-flags.handler))
+        feture-flags.handler)
+  (:require [clojure.data.json :as json]))
 
 (deftest test-app
+
+  (testing "flags route"
+    (let [response (app (request :get "/flags"))]
+      (is (= (:status response) 200))
+      (is (= (count (json/read-json (:body response))) 2 ))))
+
+  (testing "flag route"
+    (let [response (app (request :get "/flags/A"))]
+      (is (= (json/read-json (:body response)) "ON"))))
+
   (testing "main route"
     (let [response (app (request :get "/"))]
       (is (= (:status response) 200))
